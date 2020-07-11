7b3aa0fd5ba89092f2c99541fbfdba64d0469b91
Felix Geisendörfer <felix@debuggable.com>
2020-07-11 13:08:35 +0200

    Default to 30s duration like net/http/pprof
    GH-Issue: https://github.com/felixge/fgprof/issues/3

--- handler.go
+++ handler.go
@@ -6,14 +6,16 @@ import (
 	"time"
 )
 
-// Handler returns an http handler that requires a "seconds" query argument
-// and produces a profile over this duration. The optional "format" parameter
-// controls if the output is written in Google's "pprof" format (default) or
-// Brendan Gregg's "folded" stack format.
+// Handler returns an http handler that takes an optional "seconds" query
+// argument that defaults to "30" and produces a profile over this duration.
+// The optional "format" parameter controls if the output is written in
+// Google's "pprof" format (default) or Brendan Gregg's "folded" stack format.
 func Handler() http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		var seconds int
-		if _, err := fmt.Sscanf(r.URL.Query().Get("seconds"), "%d", &seconds); err != nil {
+		if s := r.URL.Query().Get("seconds"); s == "" {
+			seconds = 30
+		} else if _, err := fmt.Sscanf(s, "%d", &seconds); err != nil || seconds <= 0 {
 			w.WriteHeader(http.StatusBadRequest)
 			fmt.Fprintf(w, "bad seconds: %d: %s\n", seconds, err)
 		}
