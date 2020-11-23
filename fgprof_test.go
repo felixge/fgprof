1a39dbbc66a1a8e07716b523b259073a9be36944
Felix Geisendörfer <felix@debuggable.com>
2020-11-23 13:50:12 +0100

    Add BenchmarkProfilerGoroutines and update README

--- fgprof_test.go
+++ fgprof_test.go
@@ -2,6 +2,7 @@ package fgprof
 
 import (
 	"bytes"
+	"fmt"
 	"strings"
 	"testing"
 	"time"
@@ -28,6 +29,51 @@ func BenchmarkProfiler(b *testing.B) {
 	prof := &profiler{}
 	for i := 0; i < b.N; i++ {
 		prof.GoroutineProfile()
+	}
+}
+
+func BenchmarkProfilerGoroutines(b *testing.B) {
+	for g := 1; g <= 1024*1024; g = g * 2 {
+		g := g
+		name := fmt.Sprintf("%d goroutines", g)
+
+		b.Run(name, func(b *testing.B) {
+			prof := &profiler{}
+			initalRoutines := len(prof.GoroutineProfile())
+
+			readyCh := make(chan struct{})
+			stopCh := make(chan struct{})
+			for i := 0; i < g; i++ {
+				go func() {
+					defer func() { stopCh <- struct{}{} }()
+					readyCh <- struct{}{}
+				}()
+				<-readyCh
+			}
+
+			b.ResetTimer()
+			for i := 0; i < b.N; i++ {
+				stacks := prof.GoroutineProfile()
+				gotRoutines := len(stacks) - initalRoutines
+				if gotRoutines != g {
+					b.Logf("want %d goroutines, but got %d on iteration %d", g, len(stacks), i)
+				}
+			}
+			b.StopTimer()
+			for i := 0; i < g; i++ {
+				<-stopCh
+			}
+			start := time.Now()
+			for i := 0; ; i++ {
+				if len(prof.GoroutineProfile()) == initalRoutines {
+					break
+				}
+				time.Sleep(20 * time.Millisecond)
+				if time.Since(start) > 10*time.Second {
+					b.Fatalf("%d goroutines still running, want %d", len(prof.GoroutineProfile()), initalRoutines)
+				}
+			}
+		})
 	}
 }
 
